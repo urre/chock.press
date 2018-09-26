2273679de78971b379982344c72ee1d48e636a08
Urban Sanden <hej@urre.me>
2018-09-26 20:49:13 +0200

    Minor

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
-import Footer from './Footer';
-import Aftonbladet from './Aftonbladet';
-import Expressen from './Expressen';
+import React, { Component } from 'react'
+import Footer from './Footer'
+import Aftonbladet from './Aftonbladet'
+import Expressen from './Expressen'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <main className="context">
-          <Aftonbladet />
-          <Expressen />
-        </main>
-        <Footer />
-      </div>
-      );
-  }
+	render() {
+		return (
+			<div className="container">
+				<main className="context">
+					<Expressen />
+				</main>
+				<Footer />
+			</div>
+		)
+	}
 }
-export default App;
+export default App
