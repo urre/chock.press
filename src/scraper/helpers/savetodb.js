f0dcf8f1949cee50aa6064f5f91b149c042526b4
Urban Sanden <hej@urre.me>
2019-01-08 18:19:14 +0100

    Minor

--- src/scraper/helpers/savetodb.js
+++ src/scraper/helpers/savetodb.js
@@ -1,4 +1,3 @@
-// import GoogleSpreadsheet from 'google-spreadsheet'
 import async from 'async'
 import ab from './../../data/af.json'
 import ex from './../../data/ex.json'
@@ -12,10 +11,6 @@ const adapter = new FileSync('db.json')
 const db = low(adapter)
 
 db.defaults({ posts: [] }).write()
-
-// const doc = new GoogleSpreadsheet(
-// 	'1uniZbthuqrdPDLfa-GjVNxi8Y6OVXEcbfValKPuaeuw'
-// )
 
 var sheet
 
@@ -32,20 +27,6 @@ const send = (data, source) => {
 }
 
 async.series([
-	// function setAuth(step) {
-	// 	const creds = require('./credentials.json')
-	// 	doc.useServiceAccountAuth(creds, step)
-	// },
-	// function listRows(step) {
-	// 	doc.getInfo(function(err, info) {
-	// 		console.log('Loaded doc: ' + info.title + ' by ' + info.author.email)
-	// 		sheet = info.worksheets[0]
-	// 		console.log(
-	// 			'sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount
-	// 		)
-	// 		step()
-	// 	})
-	// },
 	function backupExpressen(step) {
 		send(ex, 'Expressen')
 		step()
@@ -53,25 +34,4 @@ async.series([
 	function backupAftonbladet(step) {
 		send(ab, 'Aftonbladet')
 	}
-
-	// function workingWithRows(step) {
-	// 	sheet.getRows(
-	// 		{
-	// 			offset: 1,
-	// 			limit: 5000,
-	// 			orderby: 'col1'
-	// 		},
-	// 		function(err, rows) {
-	// 			for (var key in rows) {
-	// 				db.get('posts')
-	// 					.push({
-	// 						title: rows[key]['heading'],
-	// 						source: rows[key]['source'],
-	// 						url: rows[key]['url']
-	// 					})
-	// 					.write()
-	// 			}
-	// 		}
-	// 	)
-	// }
 ])
