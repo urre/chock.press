b18c21f1b86496febbaeed6c44b1b802bb23fad9
Urban Sanden <hej@urre.me>
2019-01-08 13:12:08 +0100

    Update babel. Use lodwb instead of google sheet. Add circle

--- readme.md
+++ readme.md
@@ -20,22 +20,18 @@ Starts Webpack dev server at [localhost:8080](http://localhost:8080).
 
     yarn run build
 
-### Publish (scrape, build and deploy)
+### Release (scrape, build and deploy)
 
-    yarn run publish
-
-### Deploy
-
-    yarn run deploy
+    yarn run release
 
 ---
 
 ### Details
 
-* Scraper: Built using Node.js
-* Website: Built using React.js
-* Tooling: Webpack, Yarn, Babel
-* Database: Using Google Drive API to store headlines in a spreadsheet, for future use.
-* Hosted on [Now](https://zeit.co/now)
+- Scraper: Built using Node.js
+- Website: Built using React.js
+- Tooling: Webpack, Yarn, Babel
+- Database: Using Google Drive API to store headlines in a spreadsheet, for future use.
+- Hosted on [Now](https://zeit.co/now)
 
 Visit [Chockpress](https://chockpress.now.sh/)
---
+++ src/scraper/helpers/savetodb.js
@@ -0,0 +1,77 @@
+import GoogleSpreadsheet from 'google-spreadsheet'
+import async from 'async'
+import ab from './../../data/af.json'
+import ex from './../../data/ex.json'
+
+var fs = require('fs')
+
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db.defaults({ posts: [] }).write()
+
+const doc = new GoogleSpreadsheet(
+	'1uniZbthuqrdPDLfa-GjVNxi8Y6OVXEcbfValKPuaeuw'
+)
+
+var sheet
+
+const send = (data, source) => {
+	for (const key of Object.keys(data)) {
+		db.get('posts')
+			.push({
+				title: data[key].title.trim(),
+				source: source,
+				url: data[key]['url']
+			})
+			.write()
+	}
+}
+
+async.series([
+	function setAuth(step) {
+		const creds = require('./credentials.json')
+		doc.useServiceAccountAuth(creds, step)
+	},
+	function listRows(step) {
+		doc.getInfo(function(err, info) {
+			console.log('Loaded doc: ' + info.title + ' by ' + info.author.email)
+			sheet = info.worksheets[0]
+			console.log(
+				'sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount
+			)
+			step()
+		})
+	},
+	function backupExpressen(step) {
+		send(ex, 'Expressen')
+		step()
+	},
+	function backupAftonbladet(step) {
+		send(ab, 'Aftonbladet')
+	}
+
+	// function workingWithRows(step) {
+	// 	sheet.getRows(
+	// 		{
+	// 			offset: 1,
+	// 			limit: 5000,
+	// 			orderby: 'col1'
+	// 		},
+	// 		function(err, rows) {
+	// 			for (var key in rows) {
+	// 				db.get('posts')
+	// 					.push({
+	// 						title: rows[key]['heading'],
+	// 						source: rows[key]['source'],
+	// 						url: rows[key]['url']
+	// 					})
+	// 					.write()
+	// 			}
+	// 		}
+	// 	)
+	// }
+])
