12d091cd7ee880f7af5aabbef2518ce8442373e9
Urban Sandén <hej@urre.me>
2021-08-02 11:44:25 +0200

    Use RSS for Expressen as well. Remove scrape-it from scrape modules

--- src/scraper/helpers/scrape-aftonbladet.js
+++ src/scraper/helpers/scrape-aftonbladet.js
@@ -1,6 +1,5 @@
 import chalk from 'chalk'
 import fs from 'fs'
-import scrapeIt from 'scrape-it'
 import path from 'path'
 import _ from 'lodash'
 let Parser = require('rss-parser')
--- src/scraper/helpers/scrape-expressen.js
+++ src/scraper/helpers/scrape-expressen.js
@@ -1,33 +1,28 @@
 import chalk from 'chalk'
 import fs from 'fs'
-import scrapeIt from 'scrape-it'
+let Parser = require('rss-parser')
+let parser = new Parser()
 import path from 'path'
 import _ from 'lodash'
 
-const sites = ['http://www.expressen.se', 'https://www.expressen.se/noje']
+const scrapeExpressen = async () => {
+	let feed = await parser.parseURL('https://feeds.expressen.se/kvp/')
 
-const scrapeExpressen = () => {
-	for (let site of sites) {
-		scrapeIt(site, {
-			articles: {
-				listItem: '.teaser',
-				data: {
-					title: 'h2',
-					url: {
-						selector: 'a.row',
-						attr: 'href',
-					},
-				},
-			},
-		}).then(({ data, response }) => {
-			let file = path.join(__dirname, '../../data/expressen.json')
-			fs.writeFileSync(file, JSON.stringify(data), function (err) {
-				if (err) {
-					return console.log(err)
-				}
-			})
-		})
+	let articleObject = {
+		articles: [],
 	}
+
+	feed.items.forEach((item) => {
+		articleObject.articles.push({ title: item.title, url: item.link })
+	})
+
+	let file = path.join(__dirname, '../../data/expressen.json')
+
+	fs.writeFileSync(file, JSON.stringify(articleObject), function (err) {
+		if (err) {
+			return console.log(err)
+		}
+	})
 }
 
 export default scrapeExpressen
